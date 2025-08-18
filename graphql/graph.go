e602ccefd2c506b62fcf367191a40b7febe32bb2
Amritanshu05 <amritanshu0507@gmail.com>
2025-08-18 18:37:05 +0530

    graphql serv code

--- graphql/graph.go
+++ graphql/graph.go
@@ -1,58 +1,64 @@
 package main
 
+
+import (
+	"github.com/stripe/stripe-go/account"
+	"github.com/stripe/stripe-go/catalog"
+	"github.com/stripe/stripe-go/order"
+)
 type Server struct {
-// 	accountClient
-// 	catalogClient
-// 	orderClient
+	// accountClient *account.Client
+	// catalogClient *catalog.Client
+	// orderClient   *order.Client
 }
 
 func NavGraphQLServer(accountUrl, catalogUrl, orderUrl string) {*Server, error} {
-	accountClient, err := account.NewClient(accountUrl)
-		if err != nil {
-		return nil, err
-	}
+// 	accountClient, err := account.NewClient(accountUrl)
+// 		if err != nil {
+// 		return nil, err
+// 	}
 
-	catalogClient, err := catalog.NewClient(catalogUrl)
-		if err != nil {
-		accountClient.Close()
-		return nil, err
-	}
+// 	catalogClient, err := catalog.NewClient(catalogUrl)
+// 		if err != nil {
+// 		accountClient.Close()
+// 		return nil, err
+// 	}
 
 
-	orderClient, err := order.NewClient(orderUrl)
-	if err != nil {
-		accountClient.Close()
-		catalogClient.Close()
-		return nil, err
-	}
+// 	orderClient, err := order.NewClient(orderUrl)
+// 	if err != nil {
+// 		accountClient.Close()
+// 		catalogClient.Close()
+// 		return nil, err
+// 	}
 
 
 	return &Server{
-		accountClient,
-		catalogClient,
-		orderClient,
+// 		accountClient,
+// 		catalogClient,
+// 		orderClient,
 	}, nil
 }
 
-func (s *Server) Mutation() MutationResolver{
-	return &mutationResolver{
-		Server: s,
-	}
-}
+// func (s *Server) Mutation() MutationResolver{
+// 	return &mutationResolver{
+// 		Server: s,
+// 	}
+// }
 
 
-func (s *Server) Query() QueryResolver {
-	return &queryResolver{
-		server: s,
-	}
-}
+// func (s *Server) Query() QueryResolver {
+// 	return &queryResolver{
+// 		server: s,
+// 	}
+// }
 
 
-func (s *Server) Account() AccountResolver {
-	return &accountResolver{
-		server: s,
-	}
-}
+// func (s *Server) Account() AccountResolver {
+// 	return &accountResolver{
+// 		server: s,
+// 	}
+// }
 
 
 func (s *Server) ToExecutableSchema() graphql.ExecutableSchema {
